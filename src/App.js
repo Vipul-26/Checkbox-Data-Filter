2a59296628c76c4809fe4cdd219a6383cf00590c
Vipul-26 <vipulsinghssm1@gmail.com>
2022-03-13 22:00:32 +0530

    Fixed react import issue

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Header from './components/Header/index';
 import ClothingAndAccessories from './components/ClothingAndAccessories';
 import Footer from './components/Footer/index';
